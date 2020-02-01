a056d2b49da8e6146164262c34056e250b694a17
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2020-01-31 16:22:31 -0800

    Remove explanation comments to reduce bloat

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace AnimalShelter
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AnimalShelterContext>(opt =>
@@ -35,7 +35,7 @@ namespace AnimalShelter
             
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,7 +44,7 @@ namespace AnimalShelter
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
             app.UseSwagger();
